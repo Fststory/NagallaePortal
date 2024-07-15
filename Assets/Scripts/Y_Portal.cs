872945ae4873583b7acd6b3c1b4b0e586442f830
Fststory <fststory@naver.com>
2024-07-15 18:32:53 +0900

    240715 스크립트 주석 변경사항 적용

--- Assets/Scripts/Y_Portal.cs
+++ Assets/Scripts/Y_Portal.cs
@@ -20,7 +20,10 @@ public class Y_Portal : MonoBehaviour
     [SerializeField]
     private Transform testTransform;                            // 
 
-    private List<Y_CanUsePortal> portalObjects = new List<Y_CanUsePortal>();        // 
+    private List<Y_CanUsePortal> portalObjects = new List<Y_CanUsePortal>();    
+    // 포탈 사용 가능 오브젝트("이하 포사오")의 리스트, 포탈을 이용할 수 있는 자격증
+    // 포탈 사용 시에만 이 곳에 담겨 특정 기능을 수행한다
+
     public bool IsPlaced { get; private set; } = false;         // 설치 여부: 시작부터 설치돼 있지 않기에 false
     private Collider wallCollider;                              // 포탈이 설치되는 벽의 콜라이더
 
@@ -30,8 +33,8 @@ public class Y_Portal : MonoBehaviour
 
     private void Awake()
     {
-        collider = GetComponent<BoxCollider>();                 // 박스형 콜라이더
-        Renderer = GetComponent<Renderer>();                    // 렌더러
+        collider = GetComponent<BoxCollider>();                 // 박스형 콜라이더 캐싱
+        Renderer = GetComponent<Renderer>();                    // 렌더러 캐싱
     }
 
     private void Start()
@@ -45,34 +48,34 @@ public class Y_Portal : MonoBehaviour
     {
         Renderer.enabled = OtherPortal.IsPlaced;            // 짝꿍 포탈의 설치 여부가 렌더링 여부를 책임짐
 
-        for (int i = 0; i < portalObjects.Count; ++i)       // 포탈 오브젝트 리스트 길이만큼 반복
+        for (int i = 0; i < portalObjects.Count; ++i)       // 포사오 리스트 길이만큼 반복
         {
-            Vector3 objPos = transform.InverseTransformPoint(portalObjects[i].transform.position);      // 
+            Vector3 objPos = transform.InverseTransformPoint(portalObjects[i].transform.position);      // 포탈을 기준으로 한 포사오의 로컬 위치
 
-            if (objPos.z > 0.0f)
+            if (objPos.z > 0.0f)            // 포탈 중심에서 조금이라도 안으로 들어간다면
             {
-                portalObjects[i].Warp();
+                portalObjects[i].Warp();    // 해당 포사오는 워프를 시전한다.
             }
         }
     }
 
-    private void OnTriggerEnter(Collider other)         // 포탈의 트리거에 other가 닿는다면..
+    private void OnTriggerEnter(Collider other)                     // other가 포탈의 트리거에 닿는다면..
     {
-        var obj = other.GetComponent<Y_CanUsePortal>();           // obj에 other의 Y_CanUsePortal 컴포넌트를 캐싱
+        var obj = other.GetComponent<Y_CanUsePortal>();             // obj에 other의 Y_CanUsePortal 컴포넌트를 캐싱
         if (obj != null)                                            // 만약 other가 Y_CanUsePortal 컴포넌트를 갖고 있다면..
         {
-            portalObjects.Add(obj);                                 // Y_CanUsePortals 리스트에 obj를 추가한다.
+            portalObjects.Add(obj);                                 // 포사오 리스트에 obj를 추가한다.
             obj.SetIsInPortal(this, OtherPortal, wallCollider);     // 현재 맞닿아있는 포탈이 inPortal, 짝꿍이 outPortal이 된다. other과 포탈 사이의 충돌을 무시한다.
         }
     }
 
-    private void OnTriggerExit(Collider other)          // other이 포탈의 트리거에서 벗어나면..
+    private void OnTriggerExit(Collider other)                      // other이 포탈의 트리거에서 벗어나면..
     {
-        var obj = other.GetComponent<Y_CanUsePortal>();           // obj에 other의 Y_CanUsePortal 컴포넌트를 캐싱
+        var obj = other.GetComponent<Y_CanUsePortal>();             // obj에 other의 Y_CanUsePortal 컴포넌트를 캐싱
 
-        if (portalObjects.Contains(obj))                             // 만약 other가 Y_CanUsePortal 컴포넌트를 갖고 있다면
+        if (portalObjects.Contains(obj))                            // 만약 other가 Y_CanUsePortal 컴포넌트를 갖고 있다면..
         {
-            portalObjects.Remove(obj);                              // Y_CanUsePortals 리스트에서 obj를 제거한다.
+            portalObjects.Remove(obj);                              // 포사오 리스트에서 obj를 제거한다.
             obj.ExitPortal(wallCollider);                           // other과 포탈 사이의 충돌을 다시 활성화한다.
         }
     }
@@ -176,7 +179,6 @@ public class Y_Portal : MonoBehaviour
         var checkPositions = new Vector3[]
         {
             testTransform.position + testTransform.TransformVector(new Vector3( 0.0f,  0.0f, -0.1f)),
-
             testTransform.position + testTransform.TransformVector(new Vector3(-1.0f, -2.0f, -0.1f)),
             testTransform.position + testTransform.TransformVector(new Vector3(-1.0f,  2.0f, -0.1f)),
             testTransform.position + testTransform.TransformVector(new Vector3( 1.0f, -2.0f, -0.1f)),
@@ -188,8 +190,10 @@ public class Y_Portal : MonoBehaviour
         // Ensure the portal does not intersect walls.(원문)
         // 포탈이 벽과 겹치지 않도록 해줌
         var intersections = Physics.OverlapBox(checkPositions[0], checkExtents, testTransform.rotation, placementMask);
+        // 중심이 checkPositions[0], x/y/z축의 반지름?이 checkExtents, 회전 쿼터니언이 testTransform.rotation인 박스 안에 있는 물체 중
+        // 특정 레이어를 가진 물체의 콜라이더를 배열로 반환
 
-        if (intersections.Length > 1)
+        if (intersections.Length > 1)   // 포탈이 생성될 위치에 두 개 이상의 포탈 생성 가능 물체(레이어가 붙은 벽)가 감지된다면
         {
             return false;
         }
